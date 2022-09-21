7c18c6c1bdb53165ae5b217eca6c82d6ad5677fe
lsy7851 <76462754+lsy7851@users.noreply.github.com>
2022-09-22 04:22:16 +0900

    first commit

---
+++ src/test/java/com/pabi/pabieureka/PabiEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabi.pabieureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabiEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
